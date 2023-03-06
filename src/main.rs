c9ff3d18e0fc1e42c4ba764fafec4acf20f5fc52
Michael Flaherty <michaelwflaherty@me.com>
2023-03-06 14:58:57 -0800

    Remove trim

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,8 @@ async fn index(code : String, link_store : &State<LinkStore>) -> Redirect {
     let mut link_reader = link_store.store.lock().await;
     let maybe_url = link_reader.get_mut(&code);
     if let Some(url) = maybe_url {
-        let mut trimmed = url.trim().chars();
-        trimmed.next_back();
-        let url = trimmed.as_str().to_string();
-        println!("Redirecting to: '{}'", url);
-        Redirect::to(url)
+        println!("Redirecting to: '{}'", &url);
+        Redirect::to(url.clone())
     }
     else {
         Redirect::to("/")
